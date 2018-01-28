e5506d1b312b165cdd04fbcb4bced305a2ef448d
Frederik Kammel <vatbub123+github@googlemail.com>
2018-01-28 23:57:32 +0100

    Added dynamic text to the labels in the equalizer in the settings though that destroys the layout

--- app/src/main/java/com/github/vatbub/hearingaid/fragments/SettingsFragment.java
+++ app/src/main/java/com/github/vatbub/hearingaid/fragments/SettingsFragment.java
@@ -9,13 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.github.vatbub.hearingaid.R;
+import com.github.vatbub.hearingaid.RemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class SettingsFragment extends CustomFragment {
     public static final String SETTINGS_SHARED_PREFERENCES_NAME = "hearingAidSettings";
     public static final String EQ_ENABLED_PREF_KEY = "equalizerEnabled";
-    public static final boolean  EQ_ENABLED_DEFAULT_SETTING = true;
+    public static final boolean EQ_ENABLED_DEFAULT_SETTING = true;
+    public static final int numberOfChannels = 6;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -42,9 +46,10 @@ public class SettingsFragment extends CustomFragment {
         eqSwitch.setChecked(prefs.getBoolean(EQ_ENABLED_PREF_KEY, EQ_ENABLED_DEFAULT_SETTING));
 
         initButtonHandlers();
+        initFrequencyLabelsAndSeekbars();
     }
 
-    private void initButtonHandlers(){
+    private void initButtonHandlers() {
         Switch eqSwitch = findViewById(R.id.eq_on_off_switch);
         eqSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -55,9 +60,38 @@ public class SettingsFragment extends CustomFragment {
                 editor.apply();
 
                 StreamingFragment streamingFragment = (StreamingFragment) getActivity().getFragmentManager().findFragmentByTag("streamingFragment");
-                if (streamingFragment!=null)
+                if (streamingFragment != null)
                     streamingFragment.notifyEQEnabledSettingChanged();
             }
         });
+    }
+
+    private void initFrequencyLabelsAndSeekbars() {
+        double lowerFreq = FirebaseRemoteConfig.getInstance().getDouble(RemoteConfig.Keys.MIN_EQ_FREQUENCY);
+        double higherFreq = FirebaseRemoteConfig.getInstance().getDouble(RemoteConfig.Keys.MAX_EQ_FREQUENCY);
+        double hzPerChannel = (higherFreq - lowerFreq) / numberOfChannels;
+        int[] textViewIds = {R.id.text_view_bin_1, R.id.text_view_bin_2, R.id.text_view_bin_3, R.id.text_view_bin_4, R.id.text_view_bin_5, R.id.text_view_bin_6};
+
+        for (int channel = 1; channel <= numberOfChannels; channel++) {
+            double lowerBinFreq = lowerFreq + (channel - 1) * hzPerChannel;
+            double higherBinFreq = lowerFreq + channel * hzPerChannel;
+            System.out.println(lowerBinFreq);
+            System.out.println(higherBinFreq);
+            System.out.println(getString(R.string.fragment_settings_frequency_bin_pattern).replace("{lowerBinFrequency}", getStringForFrequency(lowerBinFreq)).replace("{higherBinFrequency}", getStringForFrequency(higherBinFreq)));
+
+            ((TextView) findViewById(textViewIds[channel - 1])).setText(getString(R.string.fragment_settings_frequency_bin_pattern).replace("{lowerBinFrequency}", getStringForFrequency(lowerBinFreq)).replace("{higherBinFrequency}", getStringForFrequency(higherBinFreq)));
+        }
+    }
+
+    private String getStringForFrequency(double frequency){
+        int mhzFactor = 1000000;
+        int khzFactor = 1000;
+        if (frequency>=mhzFactor){
+            return Long.toString(Math.round(frequency/mhzFactor)) + " " + getString(R.string.fragment_settings_MHz);
+        }else if(frequency>=khzFactor){
+            return Long.toString(Math.round(frequency/khzFactor)) + " " + getString(R.string.fragment_settings_kHz);
+        }else{
+            return Long.toString(Math.round(frequency)) + " " + getString(R.string.fragment_settings_Hz);
+        }
     }
 }
